61830f0583f58a90d613b9c3dcbff0004c56ebac
Ashuu312 <140257598+Ashuu312@users.noreply.github.com>
2024-07-30 12:31:26 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://whatsapp.com/channel/0029VaKyuokJUM2czbuM1M09'
+global.linkGroup = 'https://chat.whatsapp.com/ERC3b6llhjk0Kt4WKXkNov'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "Mr Asad Md";
+global.owner = ['+923014523312'];
+global.premium = ['+923014523312'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "Mr Asad (Nobita Tricks)";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
@@ -19,9 +19,9 @@ global.menuType = "2"
 global.autoswview = true
 global.autoread = false
 global.available = false
-global.autoTyping = false
-global.autoRecord = false
-global.autoreact = false
+global.autoTyping = true
+global.autoRecord = true
+global.autoreact = true
 global.onlygroup = false
 global.onlypc = false
 global.welcome = true
